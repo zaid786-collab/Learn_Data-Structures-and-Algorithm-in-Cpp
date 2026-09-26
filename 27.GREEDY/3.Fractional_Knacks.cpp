f5020942eee0afe0f7234e6facbb53a6c8f761aa
Mohammad Zaid Khan <zaidkhan24082006@gmail.com>
2026-09-26 19:57:45 +0530

    Fractional Knacks

---
+++ 27.GREEDY/3.Fractional_Knacks.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool compare(pair<double,int> p1, pair<double,int> p2)
+{
+    return p1.first > p2.first;      // Sorting Ration in Descending Order.
+}
+
+int fractionalKnapsack(vector<int> val,vector<int> wt,int W) {
+    int n = val.size();
+    vector<pair<double,int>> ratio(n,make_pair(0.0,0));
+
+    for(int i=0;i<n;i++) {
+        double r = val[i]/(double)wt[i];
+        ratio[i] = make_pair(r,i);
+    }
+
+    sort(ratio.begin(),ratio.end(), compare);
+
+    int ans = 0;
+
+    for(int i=0;i<n;i++) {
+        int index = ratio[i].second;   // Gives index of wt and val
+
+        if(wt[index] < W) {
+            ans += val[index];
+            W -= wt[index];
+        }else {
+            ans += ratio[index].first * W;
+            W = 0;
+            break;
+        }
+    }
+
+    return ans;
+}
+
+
+int main () {
+    vector<int> val = {60,100,120};
+    vector<int> wt = {10,20,30};
+    int W = 50;
+
+    cout << "Max Value in Fractional Knapsack : " << fractionalKnapsack(val,wt,W) << endl;
+    
+    return 0;
+}
